d598005e3eb70a9c0407edb816427e2f639aaf6d
Stefan Beeman <stefan@eldritchmedia.com>
2017-07-27 23:44:49 -0700

    Testing harness for tempo automation test.

---
+++ tempo/index.js
@@ -0,0 +1,6 @@
+
+function bomToJSON(bom) {
+  return bom;
+}
+
+export default bomToJSON
---
+++ tempo/test/bomToJSON.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import bomToJSON from '../index.js'
+
+const SAMPLE_INPUT = `2
+Wintermute Systems -- CASE-19201:A2,A3
+AXXX-1000:Panasonic:D1,D8,D9
+Z1,Z3;40001;Keystone
+Z1,Z3,Z8;40001;Keystone`;
+
+const SAMPLE_OUTPUT = `[
+{
+    "MPN": "40001",
+    "Manufacturer": "Keystone",
+    "ReferenceDesignators": [
+        "Z1",
+        "Z3",
+        "Z8",
+],
+    "NumOccurrences": 2,
+},
+{
+    "MPN": "AXXX-1000",
+    "Manufacturer": "Panasonic",
+    "ReferenceDesignators": [
+        "D1",
+        "D8",
+        "D9",
+],
+    "NumOccurrences": 1,
+},
+]`
+
+describe('bomToJSON', () => {
+  it('correctly translates the sample input into the sample output', () => {
+    let output = bomToJSON(SAMPLE_INPUT);
+    expect(output).to.equal(SAMPLE_OUTPUT);
+  })
+})
