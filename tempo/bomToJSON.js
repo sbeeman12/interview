a93a9ca17265b156db47ae7a383a672551b5ad50
Stefan Beeman <stefan@eldritchmedia.com>
2017-07-28 20:46:23 -0700

    Everything is working, just need to add a few small things.

--- tempo/bomToJSON.js
+++ tempo/bomToJSON.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 function parseFormatSemi(str) {
   str = str.split(';');
   return {
@@ -46,10 +48,55 @@ function bomToJSON(input) {
   // return later on.
   let n = parseInt(input.shift());
 
+  // Parse te MPNs into an intermediate format that's useful for figuring
+  // out how many of each on there is and getting an accurate tally of all
+  // of the RDs associated with each MPN.
   let inter = {};
   let mpns = input.map(parseMPN);
+  mpns.forEach((obj) => {
+    let man = obj.man;
+    let mpn = obj.mpn;
+    let rds = obj.rds;
 
-  return mpns;
+    if (!inter[man]) {
+      inter[man] = {};
+    }
+
+    if (!inter[man][mpn]) {
+      inter[man][mpn] = {
+        count: 1,
+        rds: rds
+      };
+    }
+    else {
+      inter[man][mpn].count += 1;
+      inter[man][mpn].rds = _.union(inter[man][mpn].rds, rds);
+    }
+  })
+
+  // Now we translate the intermediate format into the final format.
+  let final = []
+  _.keys(inter).forEach((man) => {
+    _.keys(inter[man]).forEach((mpn) => {
+      let obj = inter[man][mpn];
+      final.push({
+        MPN: mpn,
+        Manufacturer: man,
+        ReferenceDesignators: obj.rds,
+        NumOccurrences: obj.count
+      });
+    })
+  })
+
+  // Lastly we need to sort the final JSON objects first by NumOccurances and
+  // then by number of ReferenceDesignators.
+  final = _.sortBy(final, (obj) => {
+    // This is a quick and dirty way to sort on both criteria.
+    return (obj.NumOccurrences * -1000) - obj.ReferenceDesignators.length;
+  })
+
+  // Now we return the first N elements of the array.
+  return _.take(final, n);
 }
 
 export default bomToJSON
--- tempo/test/bomToJSON.js
+++ tempo/test/bomToJSON.js
@@ -7,7 +7,7 @@ AXXX-1000:Panasonic:D1,D8,D9
 Z1,Z3;40001;Keystone
 Z1,Z3,Z8;40001;Keystone`;
 
-const SAMPLE_OUTPUT = `[
+const SAMPLE_OUTPUT = JSON.stringify([
 {
     "MPN": "40001",
     "Manufacturer": "Keystone",
@@ -15,25 +15,24 @@ const SAMPLE_OUTPUT = `[
         "Z1",
         "Z3",
         "Z8",
-],
+    ],
     "NumOccurrences": 2,
-},
-{
+  },
+  {
     "MPN": "AXXX-1000",
     "Manufacturer": "Panasonic",
     "ReferenceDesignators": [
         "D1",
         "D8",
         "D9",
-],
+    ],
     "NumOccurrences": 1,
-},
-]`
+  }
+]);
 
 describe('bomToJSON', () => {
   it('correctly translates the sample input into the sample output', () => {
-    let output = bomToJSON(SAMPLE_INPUT);
-    console.log(output);
-    //expect(output).to.equal(SAMPLE_OUTPUT);
+    let output = JSON.stringify(bomToJSON(SAMPLE_INPUT));
+    expect(output).to.equal(SAMPLE_OUTPUT);
   })
 })
